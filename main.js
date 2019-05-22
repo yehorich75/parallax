281ea309b4b0fb7d1b428950bc72a3e4ae28fa49
Yegorich75 <yegorich75@gmail.com>
2019-05-22 09:11:56 +0300

    images errors

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ function parallax(event) {
     this.querySelectorAll('.layer').forEach(layer => {
         let speed = layer.getAttribute('data-speed');
         layer.style.transform = `translateX(${event.clientX*speed/50}px)`;
-        
     });;
-    
 }
 
 document.addEventListener('mousemove', parallax);
