410d2f07861504b05b2b862827907408247ce6a3
Yegorich75 <yegorich75@gmail.com>
2019-05-23 08:47:31 +0300

    parallax effect process

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function parallax(event) {
     this.querySelectorAll('.layer').forEach(layer => {
         let speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translateX(${event.clientX*speed/50}px)`;
-    });;
+        layer.style.transform = `translateX(${event.clientX*speed/1000}px) translateY(${event.clientY*speed/1000}px)`
+            // layer.style.transform = `translateY(${event.clientY*speed/1000}px)`
+    });
 }
 
 document.addEventListener('mousemove', parallax);
